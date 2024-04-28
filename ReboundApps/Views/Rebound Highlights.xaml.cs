46e0060863f1191605548e8f1adc715779d70882
jpb <jakub.baranski.33@gmail.com>
2024-04-28 08:35:44 +0100

    Update UI

--- ReboundApps/Views/Rebound Highlights.xaml.cs
+++ ReboundApps/Views/Rebound Highlights.xaml.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -21,9 +21,9 @@ namespace ReboundApps.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Rebound_Higlights : Page
+    public sealed partial class Rebound_Highlights : Page
     {
-        public Rebound_Higlights()
+        public Rebound_Highlights()
         {
             this.InitializeComponent();
         }
